3fdb932e81d1679bf67ca87138ded4b5f9ddb9e6
Hassan Bulega <hassanbulega@gmail.com>
2023-08-03 18:20:38 +0300

    add test for primeFactors function

--- PrimeFactorsCalculatorTests/PrimeFactorsCalculatorTests.cs
+++ PrimeFactorsCalculatorTests/PrimeFactorsCalculatorTests.cs
@@ -1,10 +1,37 @@
-namespace PrimeFactorsCalculatorTests;
+using Xunit;
+using System;
+using PrimeFactorsLibrary;
 
-public class UnitTest1
+namespace PrimeFactorsCalculatorTests
 {
-    [Fact]
-    public void Test1()
+    public class PrimeFactorsCalculatorTests
     {
+        [Fact]
+        public void TestPrimeFactorsFor2()
+        {
+            string result = PrimeFactorsCalculator.PrimeFactors(2);
+            Assert.Equal("2", result);
+        }
 
+        [Fact]
+        public void TestPrimeFactorsFor12()
+        {
+            string result = PrimeFactorsCalculator.PrimeFactors(12);
+            Assert.Equal("2 * 2 * 3", result);
+        }
+
+        [Fact]
+        public void TestPrimeFactorsFor100()
+        {
+            string result = PrimeFactorsCalculator.PrimeFactors(100);
+            Assert.Equal("2 * 2 * 5 * 5", result);
+        }
+
+        [Fact]
+        public void TestPrimeFactorsFor1()
+        {
+            string result = PrimeFactorsCalculator.PrimeFactors(1);
+            Assert.Equal("Prime factors are not defined for numbers less than 2.", result);
+        }
     }
 }
